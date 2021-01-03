215d06489301c5285bdd47163e5245678a226beb
Julio Garcia <jg@innovation.rocks>
2021-01-03 12:39:41 +0100

    added animation to car

---
+++ src/Car.ts
@@ -0,0 +1,120 @@
+import { Vector3, Vector4, StandardMaterial, Texture, MeshBuilder, Mesh, Scene, Animation, CubicEase, EasingFunction } from "@babylonjs/core";
+import earcut from 'earcut';
+
+let car: Mesh;
+let wheelRB: Mesh;
+let wheelRF: Mesh;
+let wheelLB: Mesh;
+let wheelLF: Mesh;
+let _scene: Scene;
+
+function buildCar(scene: Scene): Mesh {
+
+    _scene = scene;
+    //base
+    const outline = [
+        new Vector3(-0.3, 0, -0.1),
+        new Vector3(0.2, 0, -0.1),
+    ]
+
+    //curved front
+    for (let i = 0; i < 20; i++) {
+        outline.push(new Vector3(0.2 * Math.cos(i * Math.PI / 40), 0, 0.2 * Math.sin(i * Math.PI / 40) - 0.1));
+    }
+
+    //top
+    outline.push(new Vector3(0, 0, 0.1));
+    outline.push(new Vector3(-0.3, 0, 0.1));
+
+    //face UVs
+    const faceUV = [];
+    faceUV[0] = new Vector4(0, 0.5, 0.38, 1);
+    faceUV[1] = new Vector4(0, 0, 1, 0.5);
+    faceUV[2] = new Vector4(0.38, 1, 0, 0.5);
+
+    //material
+    const carMat = new StandardMaterial("carMat", scene);
+    carMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/car.png", scene);
+
+    car = MeshBuilder.ExtrudePolygon("car", { shape: outline, depth: 0.2, faceUV: faceUV, wrap: true }, scene, earcut);
+    car.material = carMat;
+
+    const wheelUV = [];
+    wheelUV[0] = new Vector4(0, 0, 1, 1);
+    wheelUV[1] = new Vector4(0, 0.5, 0, 0.5);
+    wheelUV[2] = new Vector4(0, 0, 1, 1);
+
+    //car material
+    const wheelMat = new StandardMaterial("wheelMat", scene);
+    wheelMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/wheel.png", scene);
+
+    wheelRB = MeshBuilder.CreateCylinder("wheelRB", { diameter: 0.125, height: 0.05, faceUV: wheelUV });
+    wheelRB.material = wheelMat;
+    wheelRB.parent = car;
+    wheelRB.position.z = -0.1;
+    wheelRB.position.x = -0.2;
+    wheelRB.position.y = 0.035;
+    wheelRF = wheelRB.clone("wheelRF");
+    wheelRF.position.x = 0.1;
+    wheelLB = wheelRB.clone("wheelLB");
+    wheelLB.position.y = -0.2 - 0.035;
+    wheelLF = wheelRF.clone("wheelLF");
+    wheelLF.position.y = -0.2 - 0.035;
+
+    setAnimations();
+
+    return car;
+}
+
+function setAnimations() {
+    const animWheel = new Animation('wheelAnimation', 'rotation.y', 30,
+        Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE);
+    const wheelKeys: Array<{ frame: number, value: number }> = [];
+    wheelKeys.push({ frame: 0, value: 0 });
+    wheelKeys.push({ frame: 30, value: 2 * Math.PI });
+    animWheel.setKeys(wheelKeys);
+    wheelRB.animations = [];
+    wheelRB.animations.push(animWheel);
+    wheelRF.animations = [];
+    wheelRF.animations.push(animWheel);
+    wheelLB.animations = [];
+    wheelLB.animations.push(animWheel);
+    wheelLF.animations = [];
+    wheelLF.animations.push(animWheel);
+
+    const ease = new CubicEase();
+    ease.setEasingMode(EasingFunction.EASINGMODE_EASEINOUT);
+
+    const animCar = new Animation("carAnimation", "position.x", 30,
+        Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE);
+    const carKeys = [];
+    carKeys.push({
+        frame: 0,
+        value: -4
+    });
+    carKeys.push({
+        frame: 150,
+        value: 4
+    });
+    carKeys.push({
+        frame: 210,
+        value: 4
+    });
+    animCar.setKeys(carKeys);
+    animCar.setEasingFunction(ease);
+    car.animations = [];
+    car.animations.push(animCar);
+}
+
+function startAnimation() {
+    _scene.beginAnimation(wheelRB, 0, 30, true);
+    _scene.beginAnimation(wheelRF, 0, 30, true);
+    _scene.beginAnimation(wheelLB, 0, 30, true);
+    _scene.beginAnimation(wheelLF, 0, 30, true);
+    _scene.beginAnimation(car, 0, 210, true);
+}
+
+export {
+    buildCar,
+    startAnimation
+}
--- src/MainScene.ts
+++ src/MainScene.ts
@@ -1,8 +1,11 @@
+import("@babylonjs/inspector");
 import {
     Scene, Engine, ArcRotateCamera, Vector3,
     HemisphericLight, SceneLoader, MeshBuilder, StandardMaterial, Color3, Texture, Vector4, Mesh
 } from '@babylonjs/core';
-import earcut from 'earcut';
+
+
+import { buildCar, startAnimation as startCarAnimation } from './Car';
 
 export class MainScene {
     private scene: Scene;
@@ -15,6 +18,7 @@ export class MainScene {
             stencil: true
         });
         this.scene = new Scene(this.engine);
+        this.scene.debugLayer.show({ embedMode: true });
         this.camera = new ArcRotateCamera('camera1', -Math.PI / 2, Math.PI / 2.5, 8, Vector3.Zero(), this.scene);
         this.camera.attachControl(canvas, true);
         this.iniScene()
@@ -32,6 +36,8 @@ export class MainScene {
                 window.addEventListener('resize', () => {
                     this.engine.resize();
                 });
+
+
             });
 
     }
@@ -39,8 +45,10 @@ export class MainScene {
     private async iniScene() {
         const ligth = new HemisphericLight('light1', new Vector3(1, 1, 0), this.scene);
         this.buildVillage();
-        const car = this.buildCar();
+        const car = buildCar(this.scene);
+        startCarAnimation();
         car.rotation.x = -Math.PI / 2;
+        car.position.z = 0.8;
         car.position.y = 0.2;
 
     }
@@ -154,57 +162,4 @@ export class MainScene {
         return roof;
     }
 
-    private buildCar = () => {
-        //base
-        const outline = [
-            new Vector3(-0.3, 0, -0.1),
-            new Vector3(0.2, 0, -0.1),
-        ]
-
-        //curved front
-        for (let i = 0; i < 20; i++) {
-            outline.push(new Vector3(0.2 * Math.cos(i * Math.PI / 40), 0, 0.2 * Math.sin(i * Math.PI / 40) - 0.1));
-        }
-
-        //top
-        outline.push(new Vector3(0, 0, 0.1));
-        outline.push(new Vector3(-0.3, 0, 0.1));
-
-        //face UVs
-        const faceUV = [];
-        faceUV[0] = new Vector4(0, 0.5, 0.38, 1);
-        faceUV[1] = new Vector4(0, 0, 1, 0.5);
-        faceUV[2] = new Vector4(0.38, 1, 0, 0.5);
-
-        //material
-        const carMat = new StandardMaterial("carMat", this.scene);
-        carMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/car.png", this.scene);
-
-        const car = MeshBuilder.ExtrudePolygon("car", { shape: outline, depth: 0.2, faceUV: faceUV, wrap: true }, this.scene, earcut);
-        car.material = carMat;
-
-        const wheelUV = [];
-        wheelUV[0] = new Vector4(0, 0, 1, 1);
-        wheelUV[1] = new Vector4(0, 0.5, 0, 0.5);
-        wheelUV[2] = new Vector4(0, 0, 1, 1);
-
-        //car material
-        const wheelMat = new StandardMaterial("wheelMat", this.scene);
-        wheelMat.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/wheel.png", this.scene);
-
-        const wheelRB = MeshBuilder.CreateCylinder("wheelRB", { diameter: 0.125, height: 0.05, faceUV: wheelUV });
-        wheelRB.material = wheelMat;
-        wheelRB.parent = car;
-        wheelRB.position.z = -0.1;
-        wheelRB.position.x = -0.2;
-        wheelRB.position.y = 0.035;
-        const wheelRF = wheelRB.clone("wheelRF");
-        wheelRF.position.x = 0.1;
-        const wheelLB = wheelRB.clone("wheelLB");
-        wheelLB.position.y = -0.2 - 0.035;
-        const wheelLF = wheelRF.clone("wheelLF");
-        wheelLF.position.y = -0.2 - 0.035;
-
-        return car;
-    }
 }
